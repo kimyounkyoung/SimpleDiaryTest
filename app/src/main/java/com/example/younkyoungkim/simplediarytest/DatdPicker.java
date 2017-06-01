538389189a6c7b64498b3dbae8100ac8bb839bd7
kimyounkyoung <passposs2@naver.com>
2017-06-01 19:03:08 +0900

    function

---
+++ app/src/main/java/com/example/younkyoungkim/simplediarytest/DatdPicker.java
@@ -0,0 +1,8 @@
+package com.example.younkyoungkim.simplediarytest;
+
+/**
+ * Created by YounKyoungKim on 2017-05-30.
+ */
+
+class DatdPicker {
+}
---
+++ app/src/main/java/com/example/younkyoungkim/simplediarytest/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.younkyoungkim.simplediarytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    DatePicker date;
+    EditText edit;
+    Button but;
+    String fileName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        date = (DatePicker) findViewById(R.id.date_pick);
+        edit = (EditText) findViewById(R.id.edit);
+        but = (Button) findViewById(R.id.but);
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DATE);
+
+        date.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int month, int day) {
+                fileName = year + "_" + (month + 1) + "_" + day + ".txt";
+                String readData = readDiary(fileName);
+                edit.setText(readData);
+                but.setEnabled(true);
+            }
+        });    //초기화
+    }
+
+    public String readDiary(String fileName) {
+        String diaryStr = null;
+        FileInputStream fIn = null;
+        try {
+            fIn = openFileInput(fileName);
+            byte[] buf = new byte[500];
+            fIn.read(buf);
+            diaryStr=new String(buf).trim();
+            but.setText("수정 하기");
+        } catch (FileNotFoundException e) {
+            edit.setText("일기가 존재하지 않습니다.");
+            but.setText("새로 저장");
+        } catch (IOException e) {
+
+        }
+        return diaryStr;
+    }
+}
